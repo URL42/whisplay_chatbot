bf9aee9913d58703fe015754843c58305390f626
JdaieLin <490844515@qq.com>
2025-06-18 16:22:36 +0800

    feat: manage sox process

--- src/device/audio.ts
+++ src/device/audio.ts
@@ -1,55 +1,63 @@
 import { exec, spawn, ChildProcess } from "child_process";
-import { noop } from "lodash";
+import { isEmpty, noop } from "lodash";
 
-let recordingProcess: ChildProcess | null = null;
+let recordingProcessList: ChildProcess[] = [];
 let currentRecordingReject: (reason?: any) => void = noop;
 
-const recordAudio = (outputPath: string, duration: number = 10): Promise<string> => {
+const killAllRecordingProcesses = (): void => {
+  recordingProcessList.forEach((process) => {
+    try {
+      process.kill();
+    } catch (e) {}
+  });
+  recordingProcessList.length = 0;
+};
+
+const recordAudio = (
+  outputPath: string,
+  duration: number = 10
+): Promise<string> => {
   return new Promise((resolve, reject) => {
     const cmd = `sox -t alsa default -t mp3 ${outputPath} silence 1 0.1 60% 1 1.0 60%`;
     console.log(`开始录音, 最长${duration}秒钟...`);
-    recordingProcess = exec(cmd, (err, stdout, stderr) => {
+    const recordingProcess = exec(cmd, (err, stdout, stderr) => {
       currentRecordingReject = reject;
       if (err) {
-        if (recordingProcess) {
-          recordingProcess.kill();
-          recordingProcess = null;
-        }
+        killAllRecordingProcesses();
         reject(stderr);
       } else {
         resolve(outputPath);
       }
     });
+    recordingProcessList.push(recordingProcess);
 
     // Set a timeout to kill the recording process after the specified duration
     setTimeout(() => {
-      if (recordingProcess) {
-        recordingProcess.kill();
-        recordingProcess = null;
+      if (recordingProcessList.includes(recordingProcess)) {
+        killAllRecordingProcesses();
         resolve(outputPath);
       }
     }, duration * 1000);
   });
 };
 
-const recordAudioManually = (outputPath: string): { result: Promise<string>; stop: () => void } => {
+const recordAudioManually = (
+  outputPath: string
+): { result: Promise<string>; stop: () => void } => {
   let stopFunc: () => void = noop;
   const result = new Promise<string>((resolve, reject) => {
     currentRecordingReject = reject;
-    recordingProcess = exec(`sox -t alsa default -t mp3 ${outputPath}`, (err, stdout, stderr) => {
-      if (err) {
-        if (recordingProcess) {
-          recordingProcess.kill();
-          recordingProcess = null;
+    const recordingProcess = exec(
+      `sox -t alsa default -t mp3 ${outputPath}`,
+      (err, stdout, stderr) => {
+        if (err) {
+          killAllRecordingProcesses();
+          reject(stderr);
         }
-        reject(stderr);
       }
-    });
+    );
     stopFunc = () => {
-      if (recordingProcess) {
-        recordingProcess.kill();
-        recordingProcess = null;
-      }
+      killAllRecordingProcesses();
       resolve(outputPath);
     };
   });
@@ -60,9 +68,8 @@ const recordAudioManually = (outputPath: string): { result: Promise<string>; sto
 };
 
 const stopRecording = (): void => {
-  if (recordingProcess) {
-    recordingProcess.kill();
-    recordingProcess = null;
+  if (!isEmpty(recordingProcessList)) {
+    killAllRecordingProcesses();
     try {
       currentRecordingReject();
     } catch (e) {}
@@ -82,7 +89,10 @@ const player: Player = {
   process: spawn("mpg123", ["-", "--scale", "2"]),
 };
 
-const playAudioData = (resAudioData: string, audioDuration: number): Promise<void> => {
+const playAudioData = (
+  resAudioData: string,
+  audioDuration: number
+): Promise<void> => {
   const audioBuffer = Buffer.from(resAudioData, "base64");
   return new Promise((resolve, reject) => {
     console.log("播放时长:", audioDuration);
@@ -140,8 +150,12 @@ process.on("SIGINT", () => {
   process.exit();
 });
 
-function splitSentences(text: string): { sentences: string[]; remaining: string } {
-  const regex = /.*?([。！？!?，,]|[\uD800-\uDBFF][\uDC00-\uDFFF]|\.)(?=\s|$)/gs;
+function splitSentences(text: string): {
+  sentences: string[];
+  remaining: string;
+} {
+  const regex =
+    /.*?([。！？!?，,]|[\uD800-\uDBFF][\uDC00-\uDFFF]|\.)(?=\s|$)/gs;
 
   const sentences: string[] = [];
   let lastIndex = 0;
@@ -171,7 +185,11 @@ class StreamResponser {
   private speakArray: Promise<{ data: string; duration: number }>[] = [];
   private parsedSentences: string[] = [];
 
-  constructor(ttsFunc: TTSFunc, sentencesCallback?: SentencesCallback, textCallback?: TextCallback) {
+  constructor(
+    ttsFunc: TTSFunc,
+    sentencesCallback?: SentencesCallback,
+    textCallback?: TextCallback
+  ) {
     this.ttsFunc = ttsFunc;
     this.sentencesCallback = sentencesCallback;
     this.textCallback = textCallback;
