048c92cbcf8d0d7c4ac4c363d9cef085adb8602e
JdaieLin <490844515@qq.com>
2025-08-09 14:27:24 +0800

    Revert "Update display.ts"
    This reverts commit c96fc4240c627f0be2480f9edb95a33c3af6a15e.

--- src/device/display.ts
+++ src/device/display.ts
@@ -59,12 +59,9 @@ export class WhisplayDisplay {
     });
     this.queueSendingInterval = setInterval(() => {
       if (this.sendingText) {
-        this.display(
-          {
-            text: this.sendingText,
-          },
-          true
-        );
+        this.display({
+          text: this.sendingText,
+        });
         this.sendingText = "";
       }
     }, 2000); // Adjust the interval as needed
@@ -192,10 +189,7 @@ export class WhisplayDisplay {
     return this.currentStatus;
   }
 
-  async display(
-    newStatus: Partial<Status> = {},
-    onlySendText: boolean = false
-  ): Promise<void> {
+  async display(newStatus: Partial<Status> = {}): Promise<void> {
     if (newStatus.text !== undefined) {
       newStatus.text = autoCropText(newStatus.text);
     }
@@ -231,9 +225,7 @@ export class WhisplayDisplay {
 
     const changedValuesObj = Object.fromEntries(changedValues);
     changedValuesObj.brightness = 100;
-    if (!onlySendText) {
-      delete changedValuesObj.text; // text is sent in a queue
-    }
+    delete changedValuesObj.text; // text is sent in a queue
     const data = JSON.stringify(changedValuesObj);
     if (isTextChanged) console.log("send data:", data);
     this.sendToDisplay(data);
